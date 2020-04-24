409d19e7fbdb52f6f27ad4d70f7f83de5a6555cf
sm1l3ycorp <35858904+sm1l3ycorp@users.noreply.github.com>
2020-04-24 10:05:16 -0500

    Update FormikDatePicker.js

--- FormikDatePicker.js
+++ FormikDatePicker.js
@@ -1,34 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import styles from '../FormikDatePicker/FormikDatePicker.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-import { useField } from 'formik';
 
-const FormikDatePicker = ({ value, onChange, onBlur, error, touched, label, name }) => {
-    // const [field, meta] = useField({ ...props, type: 'Date'});
-    // const [date, setDate] = useState(new Date());
-    const [date, setDate] = useState();
-
-    const handleDateChange = (date) => {
-        console.log(date);
-        setDate(date);
-    }
-
-    useEffect(() => {
-        setDate(date);
-    }, [date]);
-
+const FormikDatePicker = ({ placeHolderText, value, onChange, onBlur, error, touched, label, name }) => {
     return (
         <>
             <label>{label}</label>
-            <br />
+            <div className={styles.datePicker}>
             <DatePicker
-            // placeholderText='mm/dd/yyyy'
-            className='form-control'
+            placeholderText={placeHolderText}
             selected={value}
-            value={value}
             todayButton='Today'
             onChange={(value) => {
                 onChange(name, value);
@@ -37,10 +21,11 @@ const FormikDatePicker = ({ value, onChange, onBlur, error, touched, label, name
                 onBlur(name, true);
             }}
             />
+            <FontAwesomeIcon className={styles.calendarIcon} icon='calendar-alt' size='xs' />
             {touched && error ? (<div>{error}</div>) : null}
+            </div>                  
         </>
     )
 }
 
 export default FormikDatePicker;
-
